2d0732402ec64f8b1a142e2298d5a27d8bbb7a45
kamous <315008420@qq.com>
2015-11-15 22:07:13 +0800

    Initial Commit

---
+++ SubtitleParser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SubtitleParser
+//
+//  Created by kamous on 15/11/15.
+//  Copyright © 2015年 wjw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
